c68b48979c52e5cd6ef214853030c8f037a4d661
Damian Serrano Thode <mail@dsthode.info>
2019-12-05 19:19:27 +0100

    dia 1 reestructurado

--- 2019/01/src/main.rs
+++ 2019/01/src/main.rs
@@ -7,8 +7,10 @@ fn main() {
     let reader = BufReader::new(f);
     let mut masa : f32;
     let mut fuel_tmp: f32;
-    let vec_fuel_modulos : Vec<f32> = Vec::new();
-    let vec_fuel_fuel : Vec<f32> = Vec::new();
+    let fuel_modulos : f32;
+    let fuel_fuel : f32;
+    let mut vec_fuel_modulos : Vec<f32> = Vec::new();
+    let mut vec_fuel_fuel : Vec<f32> = Vec::new();
     for line in reader.lines() {
         masa = line.unwrap().parse::<f32>().unwrap();
         fuel_tmp = calcular_fuel_modulo(masa);
@@ -16,7 +18,11 @@ fn main() {
         fuel_tmp = calcular_fuel_fuel(fuel_tmp);
         vec_fuel_fuel.push(fuel_tmp);
     }
-    print!("El fuel total es: {}\n", fuel);
+    fuel_modulos = vec_fuel_modulos.iter().fold(0.0, |acc, x| acc + x);
+    fuel_fuel = vec_fuel_fuel.iter().fold(0.0, |acc, x| acc + x);
+    print!("El fuel de los modulos es: {}\n", fuel_modulos);
+    print!("El fuel del fuel es: {}\n", fuel_fuel);
+    print!("El fuel total es: {}\n", fuel_modulos + fuel_fuel);
 }
 
 fn calcular_fuel_modulo(masa:f32) -> f32 {

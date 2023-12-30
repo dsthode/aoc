54caf25172e862e22daa56cbb7ab9250c9c008ba
Damián Serrano Thode <mail@dsthode.info>
2023-12-30 14:21:30 +0000

    Refactor for 2022 day 5 part 2

--- 2022/05/src/main.rs
+++ 2022/05/src/main.rs
@@ -3,22 +3,31 @@ use std::collections::VecDeque;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+struct Move {
+  amount: u32,
+  from: usize,
+  to: usize,
+}
+
 fn main() {
-  let mut data: Vec<VecDeque<char>> = Vec::new();
+  let mut stack: Vec<VecDeque<char>> = Vec::new();
+  let mut moves: Vec<Move> = Vec::new();
   for _ in 0..9 {
-    data.push(VecDeque::new());
+    stack.push(VecDeque::new());
   }
-  let file = File::open("input.txt").unwrap();
-  for line in BufReader::new(file).lines() {
-    part1(&line.unwrap(), &mut data);
-  }
-  for v in data.into_iter() {
-    print!("{}", v.get(0).unwrap());
-  }
+  load_file(&mut stack, &mut moves);
+  part1(stack.clone(), &moves);
   println!("");
 }
 
-fn part1(line: &str, data: &mut Vec<VecDeque<char>>) {
+fn load_file(stack: &mut Vec<VecDeque<char>>, moves: &mut Vec<Move>) {
+  let file = File::open("input.txt").unwrap();
+  for line in BufReader::new(file).lines() {
+    process_line(&line.unwrap(), stack, moves);
+  }
+}
+
+fn process_line(line: &str, stack: &mut Vec<VecDeque<char>>, moves: &mut Vec<Move>) {
   let crates_rx: regex::Regex = Regex::new(
     r"(\[[A-Z]\]| {3}) (\[[A-Z]\]| {3})? ?(\[[A-Z]\]| {3})? ?(\[[A-Z]\]| {3})? ?(\[[A-Z]\]| {3})? ?(\[[A-Z]\]| {3})? ?(\[[A-Z]\]| {3})? ?(\[[A-Z]\]| {3})? ?(\[[A-Z]\]| {3})?",
   ).unwrap();
@@ -26,18 +35,18 @@ fn part1(line: &str, data: &mut Vec<VecDeque<char>>) {
   match crates_rx.captures(line) {
     Some(caps) => {
       for i in 1..=9 {
-        prepare_storage(process_match(caps.get(i)), i, data)
+        prepare_storage(process_match(caps.get(i)), i, stack)
       }
     }
     None => {}
   }
   match moves_rx.captures(line) {
-    Some(caps) => perform_move(
-      caps.get(1).unwrap().as_str().parse::<u32>().unwrap(),
-      caps.get(2).unwrap().as_str().parse::<usize>().unwrap(),
-      caps.get(3).unwrap().as_str().parse::<usize>().unwrap(),
-      data,
-    ),
+    Some(caps) => {
+      let amount = caps.get(1).unwrap().as_str().parse::<u32>().unwrap();
+      let from = caps.get(2).unwrap().as_str().parse::<usize>().unwrap();
+      let to = caps.get(3).unwrap().as_str().parse::<usize>().unwrap();
+      moves.push(Move { amount, from, to })
+    }
     None => {}
   }
 }
@@ -59,9 +68,14 @@ fn prepare_storage(chr: Option<char>, pos: usize, data: &mut Vec<VecDeque<char>>
   }
 }
 
-fn perform_move(amount: u32, from: usize, to: usize, data: &mut Vec<VecDeque<char>>) {
-  for _ in 0..amount {
-    let chr = data[from - 1].pop_front().unwrap();
-    data[to - 1].push_front(chr);
+fn part1(mut stack: Vec<VecDeque<char>>, moves: &Vec<Move>) {
+  for step in moves {
+    for _ in 0..step.amount {
+      let chr = stack[step.from - 1].pop_front().unwrap();
+      stack[step.to - 1].push_front(chr);
+    }
+  }
+  for v in stack.into_iter() {
+    print!("{}", v.get(0).unwrap());
   }
 }

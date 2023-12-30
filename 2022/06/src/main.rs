6a607066e4a4e824fc032f310cbd609f015ee86d
Damián Serrano Thode <mail@dsthode.info>
2023-12-30 15:15:17 +0000

    2022 day 6 part 2

--- 2022/06/src/main.rs
+++ 2022/06/src/main.rs
@@ -8,7 +8,8 @@ fn main() {
     panic!("Missing file name");
   }
   let data = load_file(env::args().nth(1).unwrap());
-  part1(data);
+  part1(&data);
+  part2(&data);
 }
 
 fn load_file(name: String) -> String {
@@ -19,7 +20,7 @@ fn load_file(name: String) -> String {
   return data;
 }
 
-fn part1(data: String) {
+fn part1(data: &String) {
   let the_str = data.as_str().as_bytes();
   let mut set: HashSet<u8> = HashSet::new();
   for idx in 0..data.len() - 4 {
@@ -32,6 +33,25 @@ fn part1(data: String) {
         "The marker is at position {}, so {} characters have been processed",
         idx,
         idx + 4
+      );
+      break;
+    }
+  }
+}
+
+fn part2(data: &String) {
+  let the_str = data.as_str().as_bytes();
+  let mut set: HashSet<u8> = HashSet::new();
+  for idx in 0..data.len() - 14 {
+    set.drain();
+    for count in 0..14 {
+      set.insert(the_str[idx + count]);
+    }
+    if set.len() == 14 {
+      println!(
+        "The message is at position {}, so {} characters have been processed",
+        idx,
+        idx + 14
       );
       break;
     }
